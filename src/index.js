952643ab01d79b2db8ad1fd40278ad6387eedd59
Dmitriy Smirnov <mygmbox26@gmail.com>
2022-07-13 20:49:18 +0500

    feat: implement check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketsConfigAsLine = bracketsConfig.flat().join('');
+    const bracketsStack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        const charIndex = bracketsConfigAsLine.indexOf(char);
+        if (charIndex !== -1) {
+            const bracketsConfigIndex = Math.floor(charIndex / 2);
+            const openBracket = bracketsConfig[bracketsConfigIndex][0];
+            const closeBracket = bracketsConfig[bracketsConfigIndex][1];
+            const lastStackBracket = bracketsStack[bracketsStack.length - 1];
+
+            if (bracketsStack.length === 0) {
+                bracketsStack.push(char);
+            } else if (openBracket === closeBracket) {
+                if (lastStackBracket === openBracket) {
+                    bracketsStack.pop();
+                } else {
+                    bracketsStack.push(char);
+                }
+            } else if (char === openBracket) {
+                bracketsStack.push(char);
+            } else if (char === closeBracket && lastStackBracket === openBracket) {
+                bracketsStack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return bracketsStack.length === 0;
 }
